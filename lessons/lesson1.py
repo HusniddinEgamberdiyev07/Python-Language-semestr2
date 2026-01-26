012ccab6de30c447d74b1b921005459dce949bcd
HusniddinEgamberdiyev07 <husniddin200708@gmail.com>
2026-01-26 09:36:52 +0500

    Creating new repository for second semestr

---
+++ lessons/lesson1.py
@@ -0,0 +1,11 @@
+# low level -> binary code
+# assembler level -> assembler
+# high level -> c, c++, c#, python, java
+
+# function based -> pascal, c
+# oop -> c++, java, c#
+# hybrid -> python
+
+
+# -- Recursion --
+
