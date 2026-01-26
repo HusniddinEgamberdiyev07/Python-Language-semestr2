06de1caa627903276bc04e5c8b15ef7940e3c858
HusniddinEgamberdiyev07 <husniddin200708@gmail.com>
2026-01-26 10:21:44 +0500

    lesson1 review

--- lessons/lesson1.py
+++ lessons/lesson1.py
@@ -6,6 +6,26 @@
 # oop -> c++, java, c#
 # hybrid -> python
 
+# def func_name(params):
+#   statement
 
-# -- Recursion --
+# parameter -> varaible name
+# argument -> varaible value
 
+def calc(num1=int(input("Enter a first number: ")), num2=int(input("Enter a second number: ")), operation=    input("Enter an operation: ")):
+    if(operation == "+"):   print(num1+num2)
+    elif(operation == "-"): print(num1-num2)
+    elif(operation == "*"): print(num1*num2)
+    elif(operation == "/"): print(num1/num2)
+
+# calc()
+calc(4, 2, "*")
+# calc(num2=2, operation="/", num1=10)
+
+# * takes the arguments which has no parameters
+
+def list_users(admin, *users):
+    print(admin)
+    print(users)
+
+list_users("husniddin", "nurmuhammad", "muhammad")
