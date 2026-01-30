fdbb2f0bde2c1a2f0e306444ab178ace9012c215
HusniddinEgamberdiyev07 <husniddin200708@gmail.com>
2026-01-30 06:56:34 +0500

    Half finished chatbot

---
+++ assignments/chatbot.py
@@ -0,0 +1,88 @@
+def say_hi():
+    print("Hello I am your chat bot assistant")
+
+def show_info():
+    print("info")
+
+def show_products():
+    print("Products: \n")
+    for product in shop_products:
+        print("name: ", product["name"])
+        print("category: ", product["category"])
+        print("cost: ", product["cost"])
+    print("\n")
+
+
+
+def show_single_product():
+    product_inp = input("Which one? ")
+    for product in shop_products:
+        if product["name"] == product_inp:
+            print("\nname: ", product["name"])
+            print("category: ", product["category"])
+            print("cost: ", product["cost"], "\n")
+
+            ask = input("Do you wnat to put it in bag: ")
+            if ask == "Yes":
+                add_to_bag(product)
+
+def add_to_bag(item):
+    my_bag.append(item)
+    print(my_bag)
+
+def show_bag():
+    print(my_bag)
+
+def buy_all():
+    print("bought")
+
+def run_program():
+    running = True
+    print("Shop chatbot")
+    while running:
+        question = input("What do you want to do: ")
+        if question in chatbot_data:
+            chatbot_data[question]()
+        elif question == "exit":
+            running = False
+
+my_bag = []
+
+shop_products = [
+    {
+        "name":"Hotdog",
+        "category":"food",
+        "cost":10
+    },
+    {
+        "name":"Manti",
+        "category":"food",
+        "cost":20
+    },
+    {
+        "name":"Somsa",
+        "category":"food",
+        "cost":15
+    },
+    {
+        "name":"Harry Potter",
+        "category":"book",
+        "cost":25
+    },
+    {
+        "name":"The lord of the rings",
+        "category":"book",
+        "cost":30
+    },
+]
+
+chatbot_data = {
+    "hello":say_hi,
+    "What can you do":show_info,
+    "Show me products":show_products,
+    "Show me this product": show_single_product,
+    "Show my bag":show_bag,
+    "Buy all of them":buy_all,
+}
+
+run_program()
