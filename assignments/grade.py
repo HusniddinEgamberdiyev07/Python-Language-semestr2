50804063e1cf96d2dd3b0ad8e6689d22db0094a8
HusniddinEgamberdiyev07 <husniddin200708@gmail.com>
2026-01-30 06:25:48 +0500

    Finished grading assignment

--- assignments/grade.py
+++ assignments/grade.py
@@ -5,19 +5,19 @@ grades = [
     },
     {
         "name":"technical english",
-        "grade":10
+        "grade":90
     },
     {
         "name":"technical english",
-        "grade":10
+        "grade":70
     },
     {
         "name":"technical english",
-        "grade":10
+        "grade":60
     },
     {
         "name":"technical english",
-        "grade":10
+        "grade":59
     },
 ]
 
@@ -34,11 +34,24 @@ def calc_grades(function):
             avg += grade["grade"]
         avg = avg / len(grades)
         print(avg)
-    elif(function == "passed"):
-        passed = True
-        for grade in grades:
-            if(grade["grade"] < 60):
-                passed = False
-        print(passed)
+    elif(function == "grade"):
+        passed = "passed"
+        mark = "F"
 
-calc_grades(input("Enter your function"))
+        for grade in grades:
+            if(grade["grade"] > 90):
+                mark = "A"
+            elif(grade["grade"] > 80):
+                mark = "B"
+            elif(grade["grade"] > 70):
+                mark = "C"
+            elif(grade["grade"] > 60):
+                mark = "D"
+            else:
+                mark = "F"
+                passed = "failed"
+
+            print("Subject: ", grade["name"])
+            print("Mark: ", mark, passed)
+
+calc_grades(input("Enter your function: "))
