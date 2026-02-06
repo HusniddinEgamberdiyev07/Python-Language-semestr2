c33a255ed3f3dd1e3452e302f49f0ccffe57d272
HusniddinEgamberdiyev07 <husniddin200708@gmail.com>
2026-02-06 13:37:54 +0500

    assignment for inheritance

--- assignments/class_inheritence.py
+++ assignments/class_inheritence.py
@@ -1,19 +1,54 @@
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# Incorrect inheritence but works
 
-    def walk(self):
-        print(self.name + " walking")
+class hyder:
+    def __init__(self,m,h):
+        self.m=m
+        self.h=h
+        
+    def hello(self):
+        print(self.m)
 
-class Student(Person):
-    def __init__(self, name, age, school):
-        super().__init__(name, age)
-        self.school = school
+    def bye(self):
+        print(self.h)
 
-    def study(self):
-        print("I am studying at ", self.school)
+class zafar(hyder):
+    def __init__(self,m, h, zm,zh):
+        self.zm=zm
+        self.zh=zh
+        self.m = m
+        self.h = h
 
-student1 = Student("Bot", 17, 2)
-student1.walk()
-student1.study()
+    def zjob(self):
+        print(self.zm)
+        print(self.zh)
+
+x=zafar("1000$","Gagarin", "100$", "somehwere")
+print(x.zm)
+x.bye()
+
+# Correct inheritence
+
+class hyder2:
+    def __init__(self,m,h):
+        self.m=m
+        self.h=h
+        
+    def hello(self):
+        print(self.m)
+
+    def bye(self):
+        print(self.h)
+
+class zafar2(hyder):
+    def __init__(self,m, h, zm,zh):
+        super().__init__(m, h)
+        self.zm=zm
+        self.zh=zh
+
+    def zjob(self):
+        print(self.zm)
+        print(self.zh)
+
+x2=zafar2("1000$","Gagarin", "100$", "somehwere")
+print(x2.zm)
+x2.bye()
